b9c7fc77e28df9903a7470b87512cb1dd87ba305
Singham <rahul.singh4800@gmail.com>
2019-12-14 17:12:21 +0530

    face canvas event listner

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const video = document.getElementById('video')
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandMark68Net.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExperessionNet.loadFromUri('/models')
-]).then(startVideo)
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo)
 
 function startVideo() {
     navigator.getUserMedia(
@@ -16,5 +16,11 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-    console.log('lalalal')
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval((async) => {
+        
+    }, 100);
 })
